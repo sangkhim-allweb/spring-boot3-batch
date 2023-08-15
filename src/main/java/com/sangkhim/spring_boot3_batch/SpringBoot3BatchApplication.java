a5239efe289aa6d3e5d724a45127b1107591a1a6
sangkhim <sangkhim.allweb@gmail.com>
2023-08-15 09:41:21 +0700

    execute batch every 10s

--- src/main/java/com/sangkhim/spring_boot3_batch/SpringBoot3BatchApplication.java
+++ src/main/java/com/sangkhim/spring_boot3_batch/SpringBoot3BatchApplication.java
@@ -1,38 +1,12 @@
 package com.sangkhim.spring_boot3_batch;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
-@Slf4j
-public class SpringBoot3BatchApplication implements CommandLineRunner {
-
-  private final JobLauncher jobLauncher;
-  private final Job job;
+public class SpringBoot3BatchApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBoot3BatchApplication.class, args);
-  }
-
-  @Override
-  public void run(String... args) throws Exception {
-    JobParameters jobParameters =
-        new JobParametersBuilder()
-            .addString("testParam1", "testParam1")
-            .addString("testParam2", "testParam2")
-            .toJobParameters();
-
-    log.info("BATCH STATUS :: START");
-    JobExecution execution = jobLauncher.run(job, jobParameters);
-    log.info("BATCH STATUS :: " + execution.getStatus());
   }
 }
--- src/main/java/com/sangkhim/spring_boot3_batch/scheduling/ScheduledAnnotationExample.java
+++ src/main/java/com/sangkhim/spring_boot3_batch/scheduling/ScheduledAnnotationExample.java
@@ -1,5 +1,17 @@
 package com.sangkhim.spring_boot3_batch.scheduling;
 
+import java.util.Date;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -7,13 +19,30 @@ import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource("classpath:scheduled.properties")
+@RequiredArgsConstructor
+@Slf4j
 public class ScheduledAnnotationExample {
+
+  private final JobLauncher jobLauncher;
+  private final Job job;
 
   @Async
   @Scheduled(cron = "${cron.expression}")
-  public void scheduleTaskUsingExternalizedCronExpression() {
-    System.out.println(
-        "schedule tasks using externalized cron expressions - "
-            + System.currentTimeMillis() / 1000);
+  public void scheduleTaskUsingExternalizedCronExpression()
+      throws JobInstanceAlreadyCompleteException,
+          JobExecutionAlreadyRunningException,
+          JobParametersInvalidException,
+          JobRestartException {
+
+    JobParameters jobParameters =
+        new JobParametersBuilder()
+            .addDate("date", new Date())
+            .addString("testParam1", "testParam1")
+            .addString("testParam2", "testParam2")
+            .toJobParameters();
+
+    log.info("BATCH STATUS :: START");
+    JobExecution execution = jobLauncher.run(job, jobParameters);
+    log.info("BATCH STATUS :: " + execution.getStatus());
   }
 }
